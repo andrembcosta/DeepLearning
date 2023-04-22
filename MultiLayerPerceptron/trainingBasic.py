6be1afab180ecd21d9e267434ed56b8489671e8f
Andre Macieira Braga Costa <andrembc10@gmail.com>
2023-04-22 19:26:56 -0400

    first commit

---
+++ MultiLayerPerceptron/trainingBasic.py
@@ -0,0 +1,80 @@
+### YOUR CODE HERE
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from tqdm.notebook import tqdm
+
+# Load the data
+mnist_train = datasets.MNIST(root="./datasets", train=True, transform=transforms.ToTensor(), download=True)
+mnist_test = datasets.MNIST(root="./datasets", train=False, transform=transforms.ToTensor(), download=True)
+# Make train and test sets and mini batches
+train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=100, shuffle=True)
+test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=100, shuffle=False)
+
+#Set sizes of layers
+n0 = 28*28
+n1 = 500
+n2 = 10
+
+#Initialize parameters in matrix form - with AD
+W1 = torch.randn(n0,n1)/np.sqrt(n0)
+W1.requires_grad_()
+W2 = torch.randn(n1,n2)/np.sqrt(n1)
+W2.requires_grad_()
+b1 = torch.zeros(n1, requires_grad=True)
+b2 = torch.zeros(n2, requires_grad=True)
+
+#Create optimizer
+optimizer = torch.optim.SGD([W1,W2,b1,b2], lr=0.3)
+
+#Define number of epochs
+num_epochs = 16
+
+#Perform training for multiple epochs
+for epoch in range(num_epochs):
+
+    #Perform training 
+    for images, labels in tqdm(train_loader):
+        # Zero out the gradients
+        optimizer.zero_grad()
+
+        # Flatten input x
+        x = images.view(-1, n0)
+        # Hidden layer
+        h = torch.relu(torch.matmul(x,W1)+b1)
+        # Output layer
+        y = torch.matmul(h,W2)+b2
+        # Cross-entropy
+        cross_entropy = F.cross_entropy(y, labels)
+        # Backward
+        cross_entropy.backward()
+        optimizer.step()
+
+## Testing
+correct = 0
+total = len(mnist_test)
+
+with torch.no_grad():    
+    for images, labels in tqdm(test_loader):
+
+        # Flatten input x
+        x = images.view(-1, n0)
+        # Hidden layer
+        h = torch.relu(torch.matmul(x,W1)+b1)
+        # Output layer
+        y = torch.matmul(h,W2)+b2
+        predictions = torch.argmax(y, dim=1)
+        correct += torch.sum((predictions == labels).float())
+
+# Make sure to print out your accuracy on the test set at the end.
+print('Test accuracy: {}'.format(correct/total))
+
+# | Learning Rate / #Epochs | 1     | 4     | 8     | 16    |
+# |-------------------------|-------|-------|-------|-------|
+# | 0.05                    | 91.30% | -- | -- | -- |
+# | 0.1                     | 93.00% | -- | -- | -- |
+# | 0.2                     | 94.25% | -- | -- | -- |
+# | 0.3                     | 95.20% | 97.57% | $${\color{green}\textbf{98.13}}$$ | 98.11% |
+# | 0.4                     | 94.78% | -- | -- | -- |
+      
---
+++ MultiLayerPerceptron/trainingOOP.py
@@ -0,0 +1,72 @@
+### OBJECT-ORIENTED VERSION
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision import datasets, transforms
+from tqdm.notebook import tqdm
+
+# Load the data
+mnist_train = datasets.MNIST(root="./datasets", train=True, transform=transforms.ToTensor(), download=True)
+mnist_test = datasets.MNIST(root="./datasets", train=False, transform=transforms.ToTensor(), download=True)
+# Make train and test sets and mini batches
+train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=100, shuffle=True)
+test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=100, shuffle=False)
+
+#Set sizes of layers
+n0 = 28*28
+n1 = 500
+n2 = 10
+
+class Net(nn.Module):
+    def __init__(self, n0, n1, n2):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n0, n1)
+        self.fc2 = nn.Linear(n1, n2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+#Initialize model
+model = Net(n0, n1, n2)
+
+#Define loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.3)
+
+#Define number of epochs
+num_epochs = 1
+
+#Perform training for multiple epochs
+for epoch in range(num_epochs):
+    #Perform training 
+    for images, labels in tqdm(train_loader):
+        # Zero out the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(images.view(-1, n0))
+
+        # Compute loss and backpropagation
+        loss = criterion(outputs, labels)
+        loss.backward()
+
+        # Update weights
+        optimizer.step()
+
+## Testing - OBJECT-ORIENTED VERSION
+correct = 0
+total = len(mnist_test)
+
+with torch.no_grad():    
+    for images, labels in tqdm(test_loader):
+
+        # Forward pass
+        y = model(images.view(-1, n0))
+        predictions = torch.argmax(y, dim=1)
+        correct += torch.sum((predictions == labels).float())
+
+# Make sure to print out your accuracy on the test set at the end.
+print('Test accuracy: {}'.format(correct/total))        
