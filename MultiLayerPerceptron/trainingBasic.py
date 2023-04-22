af4f44d36631842eda2f093af424f7df88494f1c
Andre Macieira Braga Costa <andrembc10@gmail.com>
2023-04-22 19:32:50 -0400

    add readme file

--- MultiLayerPerceptron/trainingBasic.py
+++ MultiLayerPerceptron/trainingBasic.py
@@ -70,11 +70,5 @@ with torch.no_grad():
 # Make sure to print out your accuracy on the test set at the end.
 print('Test accuracy: {}'.format(correct/total))
 
-# | Learning Rate / #Epochs | 1     | 4     | 8     | 16    |
-# |-------------------------|-------|-------|-------|-------|
-# | 0.05                    | 91.30% | -- | -- | -- |
-# | 0.1                     | 93.00% | -- | -- | -- |
-# | 0.2                     | 94.25% | -- | -- | -- |
-# | 0.3                     | 95.20% | 97.57% | $${\color{green}\textbf{98.13}}$$ | 98.11% |
-# | 0.4                     | 94.78% | -- | -- | -- |
+
       
